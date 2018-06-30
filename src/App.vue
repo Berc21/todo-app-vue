d62a8fa3483a1e86a3a74ac2a103b0a77a8091eb
Berkan Cetinkaya <berkan.cetinkaya@windowslive.com>
2018-06-30 22:00:04 +0300

    finish core functionality

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="app">
+
+    <todo-list/>
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+
+@import "normalize.css";
+
+
+button,
+input,
+optgroup,
+select,
+textarea,html input[type="button"],
+input[type="reset"],
+input[type="submit"],button[disabled],
+html input[disabled],button::-moz-focus-inner,
+input::-moz-focus-inner, input[type="checkbox"],
+input[type="radio"], input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button, input[type="search"], input[type="search"]::-webkit-search-cancel-button,
+input[type="search"]::-webkit-search-decoration {
+    background-image:none;
+    box-shadow: none;
+    border: none;
+    overflow: visible;
+    margin: 0;
+    padding: 0;
+}
+
+input[type="checkbox"] {
+  cursor: pointer;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  outline: 0;
+  background: lightgray;
+  height: 16px;
+  width: 16px;
+  border: 1px solid white;
+}
+
+input[type="checkbox"]:checked {
+  background: #00e676;
+}
+
+input[type="checkbox"]:hover {
+  filter: brightness(90%);
+}
+
+input[type="checkbox"]:disabled {
+  background: #e6e6e6;
+  opacity: 0.6;
+  pointer-events: none;
+}
+
+input[type="checkbox"]:after {
+  content: '';
+  position: relative;
+  left: 40%;
+  top: 20%;
+  width: 15%;
+  height: 40%;
+  border: solid #fff;
+  border-width: 0 2px 2px 0;
+  transform: rotate(45deg);
+  display: none;
+}
+
+input[type="checkbox"]:checked:after {
+  display: block;
+}
+
+input[type="checkbox"]:disabled:after {
+  border-color: #7b7b7b;
+}
+
+
+
+
+
+@import "https://fonts.googleapis.com/css?family=Roboto";
+
+body {
+  font-family: 'Roboto', sans-serif;
+}
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,293 @@
+<template>
+  <div class="todo-container">
+    <form  class="todo-form" @submit.prevent="addTodo" >
+      <input class="todo-input" type="text" @keyup.enter="addTodo" v-model="newTodoItem" placeholder="add new todo">
+      <button class="todo-button" type="submit">Add</button>
+    </form>
+    <ul class="todo-list">
+      <transition-group name="fade">
+
+
+      <li @dblclick="showEdit(item)" class="todo-list-item" v-for="(item , id) in list" :key="id">
+
+        <input class="todo-done"   v-model="item.isDone" type="checkbox" >
+
+
+
+        <div class="todo-text" v-if="item.isEditing">
+               <input class="todo-edit" type="text" @keyup.enter="editTodo(item)"  @blur="editTodo(item)" @keyup.esc="cancelEdit(item)" v-model="item.text"  v-focus >
+               <button class="todo-cancel-button" @click="cancelEdit(item)">Cancel</button>
+        </div  >
+          <span class="todo-text" :class="{ 'is-done': item.isDone }" v-else>
+                {{ item.text }}
+          </span>
+
+
+        <div class="todo-list-item-right">
+          <span @click="removeItem(id)" class="todo-list-delete">X</span>
+        </div>
+        </li>
+         </transition-group>
+    </ul>
+
+
+   <div class="todo-helpers-container">
+
+
+    <div v-if="!isEmpty">
+      <label v-if="allDone" >
+         <input type="checkbox"  checked @change="unCheckAllList"> Uncheck all <br>
+      </label>
+        <label  v-else >
+         <input type="checkbox"  @change="checkAllList" > Check All <br>
+      </label>
+    </div>
+
+  <transition name="fade">
+
+    <button class="todo-clear-done-button" v-show="showClearDone" @click="clearDone"> Clear Done</button>
+
+  </transition>
+
+
+    <p  class="todo-left" v-if="todoLeft > 0" >{{todoLeft}} Todo Left  </p>
+    <p  class="todo-left" v-else > All is done - You're free! </p>
+
+   </div>
+
+
+
+
+  </div>
+</template>
+
+<script>
+import isEmpty from "lodash.isempty";
+export default {
+  name: "TodoList",
+  data() {
+    return {
+      newTodoItem: "",
+      cachedItem: "",
+      filter: "all",
+      list: [
+        {
+          id: 1,
+          text: "Portakal Al",
+          isDone: false,
+          isEditing: false
+        },
+        {
+          id: 2,
+          text: "Elma Al",
+          isDone: false,
+          isEditing: false
+        },
+        {
+          id: 3,
+          text: "Armut Al",
+          isDone: false,
+          isEditing: false
+        },
+        {
+          id: 4,
+          text: "Salatalık Al",
+          isDone: false,
+          isEditing: false
+        }
+      ]
+    };
+  },
+  directives: {
+    focus: {
+      inserted: function(el) {
+        el.focus();
+      }
+    }
+  },
+  computed: {
+    showClearDone() {
+      return this.list.filter(item => item.isDone).length > 0;
+    },
+    todoLeft() {
+      return this.list.filter(item => !item.isDone).length;
+    },
+    allDone() {
+      return this.todoLeft == 0;
+    },
+    isEmpty() {
+      return isEmpty(this.list);
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodoItem.trim().length == 0) return;
+
+      let lastId;
+      if (!isEmpty(this.list)) {
+        lastId = this.list[this.list.length - 1].id;
+      } else {
+        lastId = -1;
+      }
+
+      let todo = {
+        id: ++lastId,
+        text: this.newTodoItem,
+        isDone: false,
+        isEditing: false
+      };
+
+      this.list.push(todo);
+
+      this.newTodoItem = "";
+    },
+    editTodo(item) {
+      if (this.newTodoItem.trim().length == 0) item.title = this.cachedItem;
+
+      item.isEditing = false;
+    },
+    removeItem(index) {
+      this.list.splice(index, 1);
+    },
+    showEdit(item) {
+      this.cachedItem = item.text;
+      item.isEditing = true;
+    },
+    cancelEdit(item) {
+      item.text = this.cachedItem;
+      item.isEditing = false;
+    },
+    clearDone() {
+      this.list = this.list.filter(item => !item.isDone);
+    },
+    checkAllList() {
+      this.list.map(item => (item.isDone = true));
+    },
+    unCheckAllList() {
+      this.list.map(item => (item.isDone = false));
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.is-done {
+  position: relative;
+  display: inline-block;
+}
+.is-done::after {
+  background: black;
+  content: "";
+  height: 0.125em;
+  right: 0;
+  left: 0;
+  position: absolute;
+  top: 50%;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 50px auto;
+  padding: 30px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  position: relative;
+}
+
+.todo-form {
+  display: flex;
+  justify-content: space-between;
+}
+
+.todo-input {
+  width: 100%;
+  max-width: 400px;
+  font-size: 18px;
+  padding: 10px 10px 10px 10px;
+  display: block;
+  border: none;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+}
+.todo-button {
+  width: 100%;
+  background-color: #67daff;
+  font-weight: bold;
+  border: none;
+  max-width: 100px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+}
+
+.todo-helpers-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.todo-clear-done-button {
+  width: 100%;
+  background-color: #00e676;
+  font-weight: bold;
+  border: none;
+  max-width: 100px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+}
+
+.todo-list {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 0;
+  margin: 20px 0;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+}
+
+.todo-list-item {
+  padding: 20px;
+  background: #90a4ae;
+  text-indent: 0;
+  list-style-type: none;
+  position: relative;
+}
+
+.todo-edit {
+  padding: 5px;
+  border-radius: 3px;
+  outline: none;
+  border: none;
+}
+
+.todo-cancel-button {
+  background-color: #c62828;
+  color: #fff;
+  border: none;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+}
+
+.todo-done {
+  display: inline-block;
+}
+.todo-text {
+  display: inline-block;
+  cursor: pointer;
+}
+
+.todo-list-item:nth-child(even) {
+  background: #37474f;
+  color: #fff;
+}
+.todo-list-delete {
+  cursor: pointer;
+  position: absolute;
+  right: 10px;
+  bottom: calc(50% - 10px);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
